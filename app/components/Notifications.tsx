e8e9723df9ee9bf63984a1e8cd6014250ef53656
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-15 21:19:03 +0400

    error fixed

--- app/components/Notifications.tsx
+++ app/components/Notifications.tsx
@@ -21,7 +21,7 @@ const NotificationsDropdown = () => {
             const response = await fetch(`https://api.muslimanshop.com/api/user/notifications/`, {
                 method: 'GET',
                 headers: {
-                    'Authorization': `${token}`,
+                    'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json',
                 },
             });
@@ -42,15 +42,17 @@ const NotificationsDropdown = () => {
     };
 
     useEffect(() => {
-        // Bildirimleri hemen çek
-        fetchNotifications();
-
-        // Her 10 saniyede bir bildirimleri çek
-        const interval = setInterval(() => {
+        // Eğer token varsa bildirimleri çekmeye başla
+        if (token) {
             fetchNotifications();
-        }, 10000);
 
-        return () => clearInterval(interval); // Temizlik
+            // Her 10 saniyede bir bildirimleri çek
+            const interval = setInterval(() => {
+                fetchNotifications();
+            }, 10000);
+
+            return () => clearInterval(interval); // Temizlik
+        }
     }, [token]);
 
     const markAllAsRead = () => {
